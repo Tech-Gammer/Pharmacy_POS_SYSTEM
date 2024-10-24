24230846768ee9af6eff21bd625ecf7410c79560
Tech Gammer <umairmughalflh@gmail.com>
2024-10-24 23:34:31 +0500

    Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'Providers/userprovider.dart';
 import 'auth_pages/Registerpage.dart';
 import 'firebase_options.dart';
 
@@ -16,9 +18,15 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pharmacy POS',
-      home: RegisterPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+
+      ],
+      child: MaterialApp(
+        title: 'Pharmacy POS',
+        home: RegisterPage(),
+      ),
     );
   }
 }
