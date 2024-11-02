c2075443587a5edc42da925335c6c1803be0916b
Tech Gammer <umairmughalflh@gmail.com>
2024-11-03 00:27:50 +0500

    only print issue

---
+++ lib/Reports/Reports for Manager/salereportbyitemsformanager.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:intl/intl.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+class SalesReportByItemPageManager extends StatefulWidget {
+  @override
+  _SalesReportByItemPageManagerState createState() => _SalesReportByItemPageManagerState();
+}
+
+class _SalesReportByItemPageManagerState extends State<SalesReportByItemPageManager> {
+  final databaseReference = FirebaseDatabase.instance.ref().child("sales");
+  DateTimeRange? selectedDateRange;
+  Map<String, Map<String, dynamic>> itemSalesData = {};
+
+  @override
+  void initState() {
+    super.initState();
+    fetchSalesData();
+  }
+
+// Fetch and aggregate sales data by item from Firebase
+  void fetchSalesData() async {
+    final snapshot = await databaseReference.get();
+    if (snapshot.value != null) {
+      Map<dynamic, dynamic> salesMap = snapshot.value as Map<dynamic, dynamic>;
+
+      // Initialize item sales data
+      Map<String, Map<String, dynamic>> tempItemSalesData = {};
+
+      salesMap.forEach((key, sale) {
+        Map<String, dynamic> saleData = Map<String, dynamic>.from(sale);
+
+        // Check if the sale date is within the selected date range
+        final dateFormat = DateFormat("dd/MM/yyyy");
+        final saleDate = dateFormat.parse(saleData['date']);
+
+        if (selectedDateRange == null ||
+            (saleDate.isAtSameMomentAs(selectedDateRange!.start) || saleDate.isAtSameMomentAs(selectedDateRange!.end) ||
+                (saleDate.isAfter(selectedDateRange!.start) && saleDate.isBefore(selectedDateRange!.end)))) {
+
+          List<dynamic> items = saleData['items'] ?? [];
+          for (var item in items) {
+            String itemName = item['name'];
+
+            // Convert quantity and price to int or double as needed
+            int itemQuantity = int.tryParse(item['quantity'].toString()) ?? 0;
+            double itemPrice = double.tryParse(item['price'].toString()) ?? 0.0;
+            double itemTotalPrice = itemPrice * itemQuantity;
+
+            if (tempItemSalesData.containsKey(itemName)) {
+              tempItemSalesData[itemName]!['totalQuantity'] += itemQuantity;
+              tempItemSalesData[itemName]!['totalSales'] += itemTotalPrice;
+            } else {
+              tempItemSalesData[itemName] = {
+                'totalQuantity': itemQuantity,
+                'totalSales': itemTotalPrice,
+              };
+            }
+          }
+        }
+      });
+
+      setState(() {
+        itemSalesData = tempItemSalesData;
+      });
+    }
+  }
+
+  // Function to show date range picker and fetch filtered sales data
+  Future<void> _selectDateRange(BuildContext context) async {
+    final DateTimeRange? picked = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != selectedDateRange) {
+      setState(() {
+        selectedDateRange = picked;
+      });
+      fetchSalesData();
+    }
+  }
+
+  // Function to generate PDF report for the item sales data
+  Future<void> generateItemSalesReportPdf() async {
+    final pdf = pw.Document();
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) => pw.Column(
+          children: [
+            pw.Text('Sales Report By Item', style: pw.TextStyle(fontSize: 24)),
+            if (selectedDateRange != null)
+              pw.Text(
+                  'Date Range: ${selectedDateRange!.start.toString().split(' ')[0]} to ${selectedDateRange!.end.toString().split(' ')[0]}'),
+            pw.SizedBox(height: 20),
+            pw.Table.fromTextArray(
+              headers: ['Item Name', 'Total Quantity', 'Total Sales'],
+              data: itemSalesData.entries.map((entry) {
+                return [
+                  entry.key,
+                  entry.value['totalQuantity'],
+                  entry.value['totalSales'].toStringAsFixed(2)
+                ];
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    await Printing.layoutPdf(
+      onLayout: (PdfPageFormat format) async => pdf.save(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: Text('Sales Report By Item'),
+        backgroundColor: Colors.teal,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.date_range),
+            onPressed: () => _selectDateRange(context),
+            tooltip: 'Select Date Range',
+          ),
+          IconButton(
+            icon: Icon(Icons.print),
+            onPressed: itemSalesData.isNotEmpty ? generateItemSalesReportPdf : null,
+            tooltip: 'Generate PDF Report',
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              selectedDateRange != null
+                  ? 'Selected Date Range: ${selectedDateRange!.start.toString().split(' ')[0]} - ${selectedDateRange!.end.toString().split(' ')[0]}'
+                  : 'No Date Range Selected',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: itemSalesData.isNotEmpty
+                  ? ListView.builder(
+                itemCount: itemSalesData.length,
+                itemBuilder: (context, index) {
+                  String itemName = itemSalesData.keys.elementAt(index);
+                  int totalQuantity = itemSalesData[itemName]!['totalQuantity'];
+                  double totalSales = itemSalesData[itemName]!['totalSales'];
+
+                  return Card(
+                    child: ListTile(
+                      title: Text('Item: $itemName'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Total Quantity Sold: $totalQuantity'),
+                          Text('Total Sales: ${totalSales.toStringAsFixed(2)}rs'),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              )
+                  : Center(child: Text('No sales data found for the selected date range')),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: itemSalesData.isNotEmpty ? generateItemSalesReportPdf : null,
+              child: Text('Generate PDF Report'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Reports/Reports for Manager/sales_report_dashboardmanager.dart
@@ -0,0 +1,126 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:pharmacy_pos_system/Reports/Reports%20for%20Manager/sale_reports.dart';
+import 'package:pharmacy_pos_system/Reports/Reports%20for%20Manager/salereportbyitemsformanager.dart';
+
+
+class SaleDashboardManager extends StatefulWidget {
+  const SaleDashboardManager({super.key});
+
+  @override
+  State<SaleDashboardManager> createState() => _SaleDashboardManagerState();
+}
+
+class _SaleDashboardManagerState extends State<SaleDashboardManager> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  User? user; // Store the user information
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  /// Build the responsive GridView based on screen width.
+  Widget _buildGridView(double screenWidth) {
+    int crossAxisCount;
+
+    // Determine the number of columns based on screen width using MediaQuery
+    if (screenWidth < 600) {
+      crossAxisCount = 2; // Mobile screens
+    } else if (screenWidth < 1200) {
+      crossAxisCount = 3; // Tablet screens
+    } else {
+      crossAxisCount = 5; // Large screens (like laptops and desktops)
+    }
+
+    return GridView.count(
+      crossAxisCount: crossAxisCount,
+      crossAxisSpacing: 20.0,
+      mainAxisSpacing: 20.0,
+      padding: const EdgeInsets.all(16.0), // Add padding if necessary
+      children: [
+        DashboardItem(
+          icon: Icons.rule,
+          label: 'Total Sales',
+          onButtonPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => SalesReportPageManager()));
+          },
+        ),
+        DashboardItem(
+          icon: Icons.rule,
+          label: 'Sales by items',
+          onButtonPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) =>   SalesReportByItemPageManager()));
+          },
+        ),
+      ],
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: const Text('Sales Report Dashboard'),
+        backgroundColor: Colors.teal,
+      ),
+      body: _buildGridView(MediaQuery.of(context).size.width), // Directly call the grid view here
+    );
+  }
+}
+
+class DashboardItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onButtonPressed;
+
+  const DashboardItem({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.onButtonPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onButtonPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.teal.shade100,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.teal,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 50,
+              color: Colors.black87,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              label,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
